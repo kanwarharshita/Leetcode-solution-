f0b8609b1cc3cf3c72649e6720e27a86420d96d7
Harshita kanwar <harshitakanwar201@gmail.com>
2025-12-16 06:53:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countPrimes(int n) {
+
+        boolean isPrime[] = new boolean [n];
+
+        for(int i=2; i<n; i++)
+        {
+            isPrime[i] = true;
+        }
+        
+        for(int i=2; i<n; i++)
+        {
+            for(int j=i*i; j<n; j=j+i)
+            {
+                isPrime[j] = false;
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 0;i<n;i++)
+        {
+            if(isPrime[i] == true)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
